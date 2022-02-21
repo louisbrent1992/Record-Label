70b63817c160f7fd998a68aacb1281858989c6a4
Louis Brent <louisbrent1992@gmail.com>
2022-02-20 20:48:18 -0800

    completed artists collage component

--- src/Components/Artist.jsx
+++ src/Components/Artist.jsx
@@ -1,5 +1,178 @@
-import React from "react";
+import { Icon } from "@iconify/react";
+import {
+	Facebook,
+	Instagram,
+	LocalGroceryStore,
+	Twitter,
+	YouTube,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default function Artist() {
-	return <div>Artist</div>;
+const Info = styled.div`
+	height: 100%;
+	width: 100%;
+	color: white;
+	position: absolute;
+	z-index: 2;
+	opacity: 0;
+	transition: all 0.5s ease;
+	background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ArtistContainer = styled.div`
+	display: flex;
+	height: 205px;
+	position: relative;
+
+	&:hover ${Info} {
+		opacity: 1;
+	}
+`;
+
+const ImgContainer = styled.div``;
+
+const Image = styled.img`
+	z-index: 1;
+	position: relative;
+	object-fit: cover;
+	width: 506px;
+	max-height: 205px;
+	margin-bottom: 5px;
+	transition: all 0.5s ease;
+`;
+
+const TopRow = styled.div`
+	display: flex;
+	height: 50%;
+	justify-content: space-between;
+`;
+const StoreIconContainer = styled.div`
+	margin: 10px;
+
+	a {
+		color: white;
+	}
+
+	a:visited {
+		color: white;
+	}
+
+	a:hover {
+		transition: color 0.2s ease-in-out;
+		color: #f5cb5c;
+		cursor: pointer;
+	}
+`;
+const Website = styled.div`
+	margin: 10px;
+	font-weight: 900;
+
+	a {
+		text-decoration: none;
+		color: white;
+	}
+
+	a:visited {
+		color: white;
+	}
+
+	a:hover {
+		transition: color 0.2s ease-in-out;
+		color: #f5cb5c;
+		cursor: pointer;
+	}
+`;
+
+const BottomRow = styled.div`
+	display: flex;
+	height: 50%;
+	justify-content: space-between;
+	align-items: flex-end;
+`;
+const Name = styled.div`
+	margin: 10px;
+	font-weight: 900;
+	color: white;
+
+	&:hover {
+		transition: color 0.2s ease-in-out;
+		color: #f5cb5c;
+		cursor: pointer;
+	}
+`;
+const SocialContainer = styled.div``;
+const SocialMediaIcons = styled.div`
+	margin: 10px 10px 5px 10px;
+	display: flex;
+	justify-content: space-between;
+	text-decoration: none;
+
+	a {
+		color: white;
+		margin-left: 5px;
+	}
+
+	a:visited {
+		color: white;
+	}
+
+	a:hover {
+		transition: color 0.2s ease-in-out;
+		color: #f5cb5c;
+		cursor: pointer;
+	}
+`;
+
+export default function Artist({ name, website, img, store, social }) {
+	return (
+		<ArtistContainer>
+			<ImgContainer>
+				<Image src={img} />
+			</ImgContainer>
+			<Info>
+				<TopRow>
+					<StoreIconContainer>
+						<Link to={store}>{store && <LocalGroceryStore />}</Link>
+					</StoreIconContainer>
+					<Website>
+						<Link to={website}>Website</Link>
+					</Website>
+				</TopRow>
+				<BottomRow>
+					<Name>{name && name}</Name>
+					<SocialContainer>
+						<SocialMediaIcons>
+							{social.Youtube.url && (
+								<Link to={social.Youtube.url}>
+									<YouTube />
+								</Link>
+							)}
+							{social.Instagram.url && (
+								<Link to={social.Instagram.url}>
+									<Instagram />
+								</Link>
+							)}
+							{social.Facebook.url && (
+								<Link to={social.Facebook.url}>
+									<Facebook />
+								</Link>
+							)}
+							{social.Twitter.url && (
+								<Link to={social.Twitter.url}>
+									<Twitter />
+								</Link>
+							)}
+
+							{social.Spotify.url && (
+								<Link to={social.Spotify.url}>
+									<Icon icon="mdi:spotify" width="25" height="25" />
+								</Link>
+							)}
+						</SocialMediaIcons>
+					</SocialContainer>
+				</BottomRow>
+			</Info>
+		</ArtistContainer>
+	);
 }
--- src/Components/ArtistsCollage.jsx
+++ src/Components/ArtistsCollage.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+import Artist from "./Artist";
 
-export default function ArtistsCollage() {
-	return <div>ArtistsCollage</div>;
+const Container = styled.div`
+	height: 100vh;
+	width: 100vw;
+	background-color: white;
+	color: #333533;
+`;
+
+const Wrapper = styled.div`
+	height: 60vh;
+	width: 100vw;
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const TitleContainer = styled.div`
+	height: 15vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+`;
+const Title = styled.h1`
+	color: #333533;
+	font-weight: 900;
+	font-size: 48px;
+	margin: 20px 0px 40px 0px;
+`;
+
+export default function ArtistsCollage({ labelArtists }) {
+	return (
+		<Container>
+			<TitleContainer>
+				<Title>Artists</Title>
+			</TitleContainer>
+			<Wrapper>
+				{labelArtists.map((artist) => (
+					<Artist
+						key={artist.id}
+						name={artist.name}
+						website={artist.website}
+						img={artist.img}
+						store={artist.store}
+						social={artist.social}
+					/>
+				))}
+			</Wrapper>
+		</Container>
+	);
 }
