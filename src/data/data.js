beeedc6978b125349deebefc319f723e3f56e158
Louis Brent <louisbrent1992@gmail.com>
2022-02-20 20:44:15 -0800

    added site data and country data

---
+++ src/data/data.js
@@ -0,0 +1,237 @@
+export const social = {
+	YoutubeAcc: { url: "youtube-url" },
+	InstagramAcc: { url: "instagram-url" },
+	FacebookAcc: { url: "facebook-url" },
+	TwitterAcc: { url: "twitter-url" },
+};
+
+export const sliderItems = [
+	{
+		id: 1,
+		img: "images/photo1.jpg",
+		title: "",
+	},
+	{
+		id: 2,
+		img: "images/photo2.jpg",
+		title: "",
+	},
+	{
+		id: 3,
+		img: "images/photo4.png",
+		title: "",
+	},
+];
+
+export const latestVideos = [
+	{
+		id: 1,
+		video: "https://youtu.be/JDLptLdVYME",
+		title: "Party G The Humble - Johnny P Freestyle [97 Piff Mix]",
+	},
+
+	{
+		id: 2,
+		video: "https://youtu.be/JDLptLdVYME",
+		title: "Artist - Music Video",
+	},
+
+	{
+		id: 3,
+		video: "https://youtu.be/JDLptLdVYME",
+		title: "Artist - Music Video",
+	},
+
+	// ],
+
+	// [
+	// 	{
+	// 		id: 4,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	// 	{
+	// 		id: 5,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	// 	{
+	// 		id: 6,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	// ],
+	// [
+	// 	{
+	// 		id: 7,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	// 	{
+	// 		id: 8,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	// 	{
+	// 		id: 9,
+	// 		video: "images/playButton.jpg",
+	// 		title: "Artist - Music Video",
+	// 	},
+	//
+];
+
+export const labelArtists = [
+	{
+		id: 1,
+		name: "",
+		img: "images/artist1.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 2,
+		name: "",
+		img: "images/artist2.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 3,
+		name: "",
+		img: "images/artist3.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 4,
+		name: "",
+		img: "images/artist4.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 5,
+		name: "",
+		img: "images/artist5.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 6,
+		name: "",
+		img: "images/artist6.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 7,
+		name: "",
+		img: "images/artist7.jpg",
+		social: {
+			Youtube: { url: "" },
+			Instagram: { url: "" },
+			Facebook: { url: "" },
+			Twitter: { url: "" },
+			Spotify: { url: "" },
+		},
+		website: "",
+		store: "",
+	},
+	{
+		id: 8,
+		name: "Stacy",
+		img: "images/artist8.jpg",
+		social: {
+			Youtube: { url: "youtube_url" },
+			Instagram: { url: "instagram_url" },
+			Facebook: { url: "facebook_url" },
+			Twitter: { url: "twitter_url" },
+			Spotify: { url: "spotify_url" },
+		},
+		website: "artist_page",
+		store: "store_page",
+	},
+	{
+		id: 9,
+		name: "Dylon",
+		img: "images/artist9.jpg",
+		social: {
+			Youtube: { url: "youtube_url" },
+			Instagram: { url: "" },
+			Facebook: { url: "facebook_url" },
+			Twitter: { url: "" },
+			Spotify: { url: "spotify_url" },
+		},
+		website: "",
+		store: "",
+	},
+];
+
+export const newReleases = [
+	{
+		id: 1,
+		title: "album_title",
+		img: "images/featured1.jpg",
+		type: "music",
+		source: "album_page",
+		desc: "album-title",
+	},
+	{
+		id: 2,
+		title: "album_title",
+		img: "images/featured2.jpg",
+		type: "music",
+		source: "album_page",
+		desc: "album_title",
+	},
+	{
+		id: 3,
+		title: "merch_title",
+		img: "images/featured3.jpg",
+		type: "merch",
+		source: "product_page",
+		desc: "merch_title",
+	},
+];
