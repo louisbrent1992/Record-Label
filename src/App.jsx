6461761c71ce88df46cf81a27f49d8fe5fcf3bb4
Louis Brent <louisbrent1992@gmail.com>
2022-02-20 20:54:17 -0800

    updated App with pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import Home from "./Pages/Home";
+import Artists from "./Pages/Artists";
+import Music from "./Pages/Music";
+import News from "./Pages/News";
+import Store from "./Pages/Store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Demos from "./Pages/Demos";
+import NoMatch from "./Pages/NoMatch";
+import Videos from "./Pages/Videos";
 
 function App() {
-	return <Home />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/artists">
+					<Artists />
+				</Route>
+				<Route path="/music">
+					<Music />
+				</Route>
+				<Route path="/videos">
+					<Videos />
+				</Route>
+				<Route path="/news">
+					<News />
+				</Route>
+				<Route path="/store">
+					<Store />
+				</Route>
+				<Route path="/demos">
+					<Demos />
+				</Route>
+				<Route>
+					<NoMatch />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
