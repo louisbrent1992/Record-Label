d339cf5c8e70a121ea4447fd44c344afa0b77415
Louis Brent <louisbrent1992@gmail.com>
2022-02-20 20:51:54 -0800

    completed home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import LatestVideos from "../Components/LatestVideos";
 import Navbar from "../Components/Navbar";
+import Slider from "../Components/Slider";
+import ArtistsCollage from "../Components/ArtistsCollage";
+import Footer from "../Components/Footer";
+import styled from "styled-components";
+import NewReleases from "../Components/NewReleases";
+import SignUp from "../Components/SignUp";
+import { labelArtists } from "../data/data";
+import { social } from "../data/data";
+
+const Container = styled.div`
+	overflow: hidden;
+`;
 
 export default function Home() {
 	return (
-		<div>
-			<Navbar />
-		</div>
+		<Container>
+			<Navbar social={social} />
+			<Slider />
+			<LatestVideos />
+			<ArtistsCollage labelArtists={labelArtists} />
+			<NewReleases />
+			<SignUp />
+			<Footer social={social} />
+		</Container>
 	);
 }
