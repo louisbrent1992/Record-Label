b28f5bec354f8485e9698ac4c1bbd377f555d502
Louis Brent <louisbrent1992@gmail.com>
2022-02-20 20:46:06 -0800

    completed navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,83 @@
-import { Facebook, Instagram, Twitter, YouTube } from "@material-ui/icons";
+import {
+	Facebook,
+	Instagram,
+	Menu,
+	Twitter,
+	YouTube,
+} from "@material-ui/icons";
+
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Icon } from "@iconify/react";
+import { tablet } from "../responsive";
 
 const Container = styled.div`
-	height: 120px;
+	height: 80px;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
 	background-color: black;
 	color: white;
+	overflow: hidden;
+	position: fixed; /* Make it stick/fixed */
+	z-index: 5;
 `;
 
 const Wrapper = styled.div`
-	padding: 50px 60px;
-	margin: 0px 20px;
+	width: 87vw;
 	display: flex;
 	align-items: center;
-	justify-content: space-between;
+	justify-content: space-around;
 `;
 
 // Left side of navbar
 const Left = styled.div`
-	flex: 1;
 	display: flex;
 	align-items: center;
-	justify-content: flex-start;
 `;
 const Logo = styled.h1`
-	font-weight: bold;
+	font-weight: 900;
+	font-size: 48px;
+`;
+
+const MenuContainer = styled.div`
+	${tablet({ display: "none" })}
 `;
 
 // Center of navbar
 const Center = styled.div`
-	flex: 1;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
 `;
-const Link = styled.div`
+const NavLink = styled.div`
 	font-size: 18px;
-	font-weight: bold;
+	font-weight: 900;
 	cursor: pointer;
-	margin-left: 25px;
+	margin-left: 50px;
 
-	&:hover {
-		transition: all 1.8s ease-in-out 1s;
+	a {
+		text-decoration: none;
+	}
+
+	a:visited {
+		color: white;
+	}
+
+	a:hover {
+		transition: all 0.2s ease-in-out;
 		text-decoration: underline;
 		text-decoration-thickness: 4px;
 		text-decoration-style: solid;
 		text-underline-offset: 0.5rem;
+		color: #f5cb5c;
 	}
 `;
 
 // Right side of navbar
 const Right = styled.div`
-	flex: 1;
 	display: flex;
 	align-items: center;
-	justify-content: flex-end;
 `;
 const SocialIcon = styled.div`
 	width: 40px;
@@ -63,37 +85,78 @@ const SocialIcon = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+
+	a {
+		color: white;
+	}
+
+	a:visited {
+		color: white;
+	}
+
+	a:hover {
+		transition: color 0.2s ease-in-out;
+		color: #f5cb5c;
+		cursor: pointer;
+	}
 `;
 
-export default function Navbar() {
+export default function Navbar({ social }) {
+	const { YoutubeAcc, InstagramAcc, FacebookAcc, TwitterAcc } = social;
 	return (
 		<Container>
 			<Wrapper>
 				<Left>
+					<MenuContainer>
+						<SocialIcon>
+							<Menu />
+						</SocialIcon>
+					</MenuContainer>
 					<Logo>SAB</Logo>
 				</Left>
 				<Center>
-					<Link>Home</Link>
-					<Link>Artists</Link>
-					<Link>News</Link>
-					<Link>Music</Link>
-					<Link>Store</Link>
+					<NavLink>
+						<Link to="/">Home</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/artists">Artists</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/music">Music</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/videos">Videos</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/news">News</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/store">Store</Link>
+					</NavLink>
+					<NavLink>
+						<Link to="/demos">Demo Submissions</Link>
+					</NavLink>
 				</Center>
 				<Right>
 					<SocialIcon>
-						<YouTube />
+						<Link to={FacebookAcc.url}>
+							<Facebook />
+						</Link>
 					</SocialIcon>
 					<SocialIcon>
-						<Facebook />
+						<Link to={TwitterAcc.url}>
+							<Twitter />
+						</Link>
 					</SocialIcon>
 					<SocialIcon>
-						<Instagram />
+						<Link to={InstagramAcc.url}>
+							<Instagram />
+						</Link>
 					</SocialIcon>
 					<SocialIcon>
-						<Twitter />
-					</SocialIcon>
-					<SocialIcon>
-						<Icon icon="mdi:spotify" width="25" height="25" />
+						<Link to={YoutubeAcc.url}>
+							<YouTube />
+						</Link>
 					</SocialIcon>
 				</Right>
 			</Wrapper>
