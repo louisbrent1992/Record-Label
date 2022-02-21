b88d4537f9c49d1a8869e26013c3d7f5a531301b
Louis Brent <louisbrent1992@gmail.com>
2022-02-21 09:39:14 -0800

    updated responsive layout settings

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
 	return css`
-		@media only screen and (max-width: 450px) {
+		@media only screen and (max-width: 35rem) {
 			${props}
 		}
 	`;
@@ -10,7 +10,15 @@ export const mobile = (props) => {
 
 export const tablet = (props) => {
 	return css`
-		@media only screen and (min-width: 450px) {
+		@media only screen and (min-width: 35rem) and (max-width: 55rem) {
+			${props}
+		}
+	`;
+};
+
+export const desktop = (props) => {
+	return css`
+		@media only screen and (min-width: 55rem) {
 			${props}
 		}
 	`;
