e9cf399e5d6606d0e5f0a21c998125996c2c0a56
Louis Brent <louisbrent1992@gmail.com>
2022-02-24 09:52:10 -0800

    added tablet view setting in responsive

--- src/responsive.js
+++ src/responsive.js
@@ -8,6 +8,14 @@ export const mobile = (props) => {
 	`;
 };
 
+export const tablet = (props) => {
+	return css`
+		@media only screen and (min-width: 32rem) and (max-width: 55rem) {
+			${props}
+		}
+	`;
+};
+
 export const desktop = (props) => {
 	return css`
 		@media only screen and (min-width: 55rem) {
