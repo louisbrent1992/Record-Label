81776863602c86347b6b4fca30cd352193255e1a
Louis Brent <louisbrent1992@gmail.com>
2022-02-23 18:06:21 -0800

    updated navbar for mobile response

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,70 +1,63 @@
-import {
-	Facebook,
-	Instagram,
-	Menu,
-	Twitter,
-	YouTube,
-} from "@material-ui/icons";
+import { Facebook, Instagram, Twitter, YouTube } from "@material-ui/icons";
 
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { tablet } from "../responsive";
+import { Link } from "react-router-dom";
+import { desktop, mobile } from "../responsive";
 
-const Container = styled.div`
-	height: 80px;
-	width: 100vw;
-	display: flex;
-	justify-content: center;
+const NavbarContainer = styled.nav`
+	width: 100%;
+	height: ${(props) => (props.extendNavbar ? "100vh" : "80px")};
 	background-color: black;
-	color: white;
-	overflow: hidden;
-	position: fixed; /* Make it stick/fixed */
-	z-index: 5;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: fixed;
+	z-index: 9;
+
+	${desktop({ height: "80px" })};
 `;
 
-const Wrapper = styled.div`
+const LeftContainer = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const RightContainer = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 3rem;
+	margin-left: 2rem;
+`;
+
+const NavbarInnerContainer = styled.div`
 	width: 87vw;
+	height: 80px;
 	display: flex;
-	align-items: center;
-	justify-content: space-around;
+	position: fixed;
+	z-index: 9;
+	gap: 3rem;
+	justify-content: center;
+
+	${mobile({ width: "100%" })}
 `;
 
-// Left side of navbar
-const Left = styled.div`
+const NavbarLinkContainer = styled.div`
 	display: flex;
-	align-items: center;
-`;
-const Logo = styled.h1`
-	font-weight: 900;
-	font-size: 48px;
+	gap: 1rem;
 `;
 
-const MenuContainer = styled.div`
-	${tablet({ display: "none" })}
-`;
-
-// Center of navbar
-const Center = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-`;
-const NavLink = styled.div`
-	font-size: 18px;
-	font-weight: 900;
-	cursor: pointer;
-	margin-left: 50px;
-
-	a {
-		text-decoration: none;
-	}
+const NavbarLink = styled(Link)`
+	color: white;
+	font-size: 20px;
+	font-family: Arial, Helvetica, sans-serif;
+	text-decoration: none;
 
 	a:visited {
 		color: white;
 	}
 
-	a:hover {
+	&:hover {
 		transition: all 0.2s ease-in-out;
 		text-decoration: underline;
 		text-decoration-thickness: 4px;
@@ -72,20 +65,81 @@ const NavLink = styled.div`
 		text-underline-offset: 0.5rem;
 		color: #f5cb5c;
 	}
+
+	${mobile({ display: "none" })};
 `;
 
-// Right side of navbar
-const Right = styled.div`
+const NavbarLinkExtended = styled(Link)`
+	color: white;
+	font-size: 20px;
+	font-family: Arial, Helvetica, sans-serif;
+	text-decoration: none;
+
+	a:visited {
+		color: white;
+	}
+
+	&:hover {
+		transition: all 0.2s ease-in-out;
+		color: #f5cb5c;
+	}
+`;
+
+const Logo = styled.h1`
+	color: white;
+	font-size: 48px;
+	font-weight: 900;
+
+	${mobile({
+		position: "absolute",
+		top: "0",
+		left: "0",
+		marginLeft: "1rem",
+		marginTop: "0.5rem",
+	})}
+`;
+
+const OpenLinksButton = styled.button`
+	width: 60px;
+	height: 50px;
+	padding-top: 0.5rem;
+	background: none;
+	border: none;
+	color: white;
+	font-size: 45px;
+	cursor: pointer;
+	position: absolute;
+	top: 0;
+	right: 0;
+
+	${desktop({ display: "none" })};
+`;
+
+const NavbarExtendedContainer = styled.div`
 	display: flex;
 	align-items: center;
+	flex-direction: column;
+	gap: 2rem;
+	height: 100vh;
+	margin-top: 175px;
+
+	${desktop({ display: "none" })};
+`;
+
+const ExtendedSocialIconsContainer = styled.div`
+	color: white;
+	display: flex;
+	gap: 1rem;
+`;
+
+const SocialIconsContainer = styled.div`
+	color: white;
+	display: flex;
+	gap: 1rem;
+
+	${mobile({ display: "none" })};
 `;
 const SocialIcon = styled.div`
-	width: 40px;
-	height: 40px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
 	a {
 		color: white;
 	}
@@ -103,63 +157,87 @@ const SocialIcon = styled.div`
 
 export default function Navbar({ social }) {
 	const { YoutubeAcc, InstagramAcc, FacebookAcc, TwitterAcc } = social;
+
+	const [extendNavbar, setExtendNavbar] = useState(false);
+
 	return (
-		<Container>
-			<Wrapper>
-				<Left>
-					<MenuContainer>
-						<SocialIcon>
-							<Menu />
-						</SocialIcon>
-					</MenuContainer>
+		<NavbarContainer extendNavbar={extendNavbar}>
+			<NavbarInnerContainer>
+				<LeftContainer>
 					<Logo>SAB</Logo>
-				</Left>
-				<Center>
-					<NavLink>
-						<Link to="/">Home</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/artists">Artists</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/music">Music</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/videos">Videos</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/news">News</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/store">Store</Link>
-					</NavLink>
-					<NavLink>
-						<Link to="/demos">Demo Submissions</Link>
-					</NavLink>
-				</Center>
-				<Right>
-					<SocialIcon>
-						<Link to={FacebookAcc.url}>
-							<Facebook />
-						</Link>
-					</SocialIcon>
-					<SocialIcon>
-						<Link to={TwitterAcc.url}>
-							<Twitter />
-						</Link>
-					</SocialIcon>
-					<SocialIcon>
-						<Link to={InstagramAcc.url}>
-							<Instagram />
-						</Link>
-					</SocialIcon>
-					<SocialIcon>
-						<Link to={YoutubeAcc.url}>
-							<YouTube />
-						</Link>
-					</SocialIcon>
-				</Right>
-			</Wrapper>
-		</Container>
+				</LeftContainer>
+				<RightContainer>
+					<NavbarLinkContainer>
+						<NavbarLink to="/"> Home</NavbarLink>
+						<NavbarLink to="/music">Music</NavbarLink>
+						<NavbarLink to="/videos"> Videos</NavbarLink>
+						<NavbarLink to="/news"> News</NavbarLink>
+						<NavbarLink to="/store"> Store</NavbarLink>
+						<NavbarLink to="/demos"> Demo Submissions</NavbarLink>
+						<OpenLinksButton
+							onClick={() => {
+								setExtendNavbar((curr) => !curr);
+							}}
+						>
+							{extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+						</OpenLinksButton>
+					</NavbarLinkContainer>
+					<SocialIconsContainer>
+						<SocialIcon>
+							<Link to={FacebookAcc.url}>
+								<Facebook />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={TwitterAcc.url}>
+								<Twitter />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={InstagramAcc.url}>
+								<Instagram />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={YoutubeAcc.url}>
+								<YouTube />
+							</Link>
+						</SocialIcon>
+					</SocialIconsContainer>
+				</RightContainer>
+			</NavbarInnerContainer>
+			{extendNavbar && (
+				<NavbarExtendedContainer>
+					<NavbarLinkExtended to="/"> Home</NavbarLinkExtended>
+					<NavbarLinkExtended to="/music">Music</NavbarLinkExtended>
+					<NavbarLinkExtended to="/videos"> Videos</NavbarLinkExtended>
+					<NavbarLinkExtended to="/news"> News</NavbarLinkExtended>
+					<NavbarLinkExtended to="/store"> Store</NavbarLinkExtended>
+					<NavbarLinkExtended to="/demos"> Demos</NavbarLinkExtended>
+					<ExtendedSocialIconsContainer style={{ bottom: "0" }}>
+						<SocialIcon>
+							<Link to={FacebookAcc.url}>
+								<Facebook />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={TwitterAcc.url}>
+								<Twitter />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={InstagramAcc.url}>
+								<Instagram />
+							</Link>
+						</SocialIcon>
+						<SocialIcon>
+							<Link to={YoutubeAcc.url}>
+								<YouTube />
+							</Link>
+						</SocialIcon>
+					</ExtendedSocialIconsContainer>
+				</NavbarExtendedContainer>
+			)}
+		</NavbarContainer>
 	);
 }
